6bcef66d88f8386869880689523907189adbee5d
NickyDz <nickydcruz@yahoo.com>
2018-12-26 22:07:41 +0530

    Initial Commit

---
+++ app/index.js
@@ -0,0 +1,32 @@
+/*
+*
+* Primary File for the API
+*
+* Homework Assignment 1 - Simple Node server that responds to hello request with json response
+*
+*
+*/
+
+var http = require('http');
+const url = require('url');
+
+// Create server
+var server = http.createServer(function(req,res){
+
+  // get parsedUrl from request
+  var parsedUrl = url.parse(req.url,true);
+
+// get Path from url
+var path = parsedUrl.pathname;
+var trimmedPath = path.replace(/^\/+|\/+$/g,'');
+
+
+
+  res.end("Hello matey\n");
+})
+
+
+//Listen on server
+server.listen(3000,function(){
+  console.log("Listening on port ");
+})
